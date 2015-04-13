4909b466fc8dc640e77daf1094d12c95665b387d
joesbeyond <joesbeyond@gmail.com>
2015-04-13 22:30:47 +0800

    update

--- testPort.js
+++ testPort.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text-plain' });
     response.end('Hello World\n');
-}).listen(8535);
+}).listen(8123);
